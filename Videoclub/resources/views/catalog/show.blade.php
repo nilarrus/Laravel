653a5578478af067017f51b66927b0a69b762ec5
super <usuari@domini.com>
2019-02-14 16:34:25 +0100

    super guay

--- Videoclub/resources/views/catalog/show.blade.php
+++ Videoclub/resources/views/catalog/show.blade.php
@@ -7,13 +7,15 @@
     <div class="col-sm-4">
 
         {{-- TODO: Imagen de la película --}}
-        <img src="{{$pelicula['poster']}}" style="height:200px"/>
+        <img src="{{$pelicula['poster']}}"/>
 
     </div>
     <div class="col-sm-8">
-
         {{-- TODO: Datos de la película título, año, director, resumen y su estado   --}}
-
+        <h2>{{$pelicula['title']}}</h2>
+        <h4>Año:{{$pelicula['year']}}</h4>
+        <p>Resumen:{{$pelicula['synopsis']}}</p>
+        <p>Estado:{{$pelicula['rented']}}</p>
     </div>
 </div>	
 @stop
